eaa0ed204e756b3af831e0a98368ee95ad072beb
Edanur AKTAN <edanur.aktann@gmail.com>
2025-06-04 00:35:01 +0300

    sayaç düzenlendi

--- src/components/Zikirmatik.jsx
+++ src/components/Zikirmatik.jsx
@@ -229,7 +229,7 @@ const Zikirmatik = () => {
 
       {/* Zikirmatik */}
       <div style={containerStyle}>
-        <h2 style={{ marginTop: "0.3rem", marginBottom: "1rem", fontSize: 70 }}>⏱️{selectedTarget.text}</h2>
+        <h2 style={{ marginTop: "0.3rem", marginBottom: "1rem", fontSize: 70 }}>{selectedTarget.text}</h2>
 
         <label>
            Hedef:
